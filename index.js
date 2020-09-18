60f3fb982c7f801aa39aa24ec79fd96616516445
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2020-09-18 15:24:52 -0300

    chore: create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const jestConfig = require('./jest.config'),
+  eslintConfig = require('./.eslintrc'),
+  prettierConfig = require('./prettier.config');
+
+module.exports = {
+  jestConfig,
+  eslintConfig,
+  prettierConfig
+};
